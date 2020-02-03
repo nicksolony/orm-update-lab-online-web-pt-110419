211c70c18f2728ca7a54b90ed3f811ffebf0f671
Nick Solonyy <nicksolony@gmail.com>
2020-02-03 05:07:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,7 @@ class Student
   
   def self.find_by_name(name)
     #This class method takes in an argument of a name
-    sql = "SELECT * FROM students WHERE name = ?;"
+    sql = "SELECT * FROM students WHERE name = ? LIMIT 1;"
     row = DB[:conn].execute(sql,name).first
     self.new_from_db(row)
   
