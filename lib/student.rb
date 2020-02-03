67ff58e0cfb22245b12208c3acceb55b3845f9a1
Nick Solonyy <nicksolony@gmail.com>
2020-02-03 04:52:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,9 @@ class Student
           INSERT INTO students (name, grade)
           VALUES(?,?);
           SQL
-    DB[:conn].execute(sql,self.name,self,grade)      
+    DB[:conn].execute(sql,self.name,self,grade) 
+    
+    @id = DB[:conn].execute("SELECT id ") 
   end
   
   
