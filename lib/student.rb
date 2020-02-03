cae89c1bc7f0dedc6ae20ebdd0fb45803175b834
Nick Solonyy <nicksolony@gmail.com>
2020-02-03 05:04:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,6 +59,9 @@ class Student
     self.new(row[1],row[2],row[0])
   end
   
+  def self.find_by_name
+    #
+  
   
   def update
     #This method updates the database row mapped to the given Student instance.
